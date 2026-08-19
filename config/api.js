391d4f2b5246c39111b6eb6920f18d2deb1a1f2c
gustav080gf-boop <gustav080.gf@gmail.com>
2026-08-18 23:36:34 -0300

    Conectar GitHub Pages con Apps Script

--- config/api.js
+++ config/api.js
@@ -1,1 +1,1 @@
-window.GF_API_URL = 'REEMPLAZAR_URL_APPS_SCRIPT';
+window.GF_API_URL = 'https://script.google.com/macros/s/AKfycby439aU7A0nxKXK3vcZfA6rbuEdip7TfQaa2P2TqUy7k64GBSiatenxKGhOqPsir9U_gA/exec';
